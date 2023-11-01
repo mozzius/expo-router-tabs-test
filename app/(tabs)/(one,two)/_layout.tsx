114e337df5a0c4cae6190440ee5491a2eb51e214
Samuel Newman <mozzius@protonmail.com>
2023-11-01 12:55:13 +0000

    explicit substack routes

--- app/(tabs)/(one,two)/_layout.tsx
+++ app/(tabs)/(one,two)/_layout.tsx
@@ -1,7 +1,19 @@
 import { Stack } from "expo-router";
 
+// add this has no effect:
+// export const unstable_settings = {
+//   initialRouteName: "one",
+//   two: {
+//     initialRouteName: "two",
+//   },
+// };
+
 export default function Substack() {
   return (
-    <Stack />
+    <Stack>
+      <Stack.Screen name="one" options={{ title: "One" }} />
+      <Stack.Screen name="two" options={{ title: "Two" }} />
+      <Stack.Screen name="three" options={{ title: "Three" }} />
+    </Stack>
   );
 }
